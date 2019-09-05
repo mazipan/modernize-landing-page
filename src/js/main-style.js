89292e4cd8683aafc29d30538de2c36848da5ab8
Irfan Maulana <maulana.irfan@tokopedia.com>
2019-09-05 19:03:38 +0700

    feat: move to npm

---
+++ src/js/main-style.js
@@ -0,0 +1,8 @@
+
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'magnific-popup/dist/magnific-popup.css'
+import 'swiper/dist/css/swiper.css'
+
+import '../scss/main.scss'
+import '../scss/custom.scss'
